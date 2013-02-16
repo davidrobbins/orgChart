00699abcbafc7dbfcfca08b944d10a300f460d3a
David Robbins <dave@wakanda.org>
2013-02-16 09:28:37 -0800

    updated index page.

--- orgChart/WebFolder/index.waPage/scripts/index.js
+++ orgChart/WebFolder/index.waPage/scripts/index.js
@@ -2,10 +2,16 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
 	var login1 = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
 
 	login1.logout = function login1_logout (event)// @startlock
 	{// @endlock
@@ -14,10 +20,11 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	login1.login = function login1_login (event)// @startlock
 	{// @endlock
-		waf.sources.employee.all();
+		waf.sources.employee.all({autoExpand: "reportsTo"});
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("login1", "logout", login1.logout, "WAF");
 	WAF.addListener("login1", "login", login1.login, "WAF");
 // @endregion
