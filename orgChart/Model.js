246f3bdb552b075c1b43755ee39e78f42f93beac
David Robbins <dave@wakanda.org>
2013-02-16 06:50:49 -0800

    added index page and restricting query.

--- orgChart/Model.js
+++ orgChart/Model.js
@@ -3,6 +3,24 @@ guidedModel =// @startlock
 {
 	Employee :
 	{
+		events :
+		{
+			onRestrictingQuery:function()
+			{// @endlock
+				var utility= require('utility');
+				if(currentSession().belongsTo(__myNameSpace.DirectoryROLES.ADMINISTRATOR)){
+					return this.all();
+				} else if (!sessionStorage.ID) {
+					return this.createEntityCollection();
+				} else {
+					var myUser = ds.Employee.find("email = :1", currentSession().user.name);
+					if (myUser != null) 
+						return utility.getSubordinates(myUser);
+					else
+						return this.createEntityCollection();
+				}
+			}// @startlock
+		},
 		entityMethods :
 		{// @endlock
 			validatePassword:function(password)
